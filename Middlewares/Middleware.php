ba30eddeafdddee6d9e3eefe1150a9979264bbe2
Francis Carbah <fcarbah@gmail.com>
2019-08-18 23:10:03 -0400

    backend updates

---
+++ Middlewares/Middleware.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Feather\Ignite\Middlewares;
+use Feather\Init\Middleware;
+/**
+ * Description of Middleware
+ *
+ * @author fcarbah
+ */
+class Middleware extends Middleware {
+    //put your code here
+}
