e8c08a2c547c6156a495d28e9985841179a50c20
fcarbah <fcarbah@yahoo.com>
2019-08-26 09:19:51 -0400

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -3,3 +3,4 @@ define("ABS_PATH", dirname($_SERVER['DOCUMENT_ROOT']));
 define("STORAGE_PATH",ABS_PATH.'/storage/');
 define("VIEWS_PATH", $_SERVER['DOCUMENT_ROOT'].'/views/');
 define("PUBLIC_PATH", $_SERVER['DOCUMENT_ROOT']);
+define('AUTH_USER','feather_auth_user');
