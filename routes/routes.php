45c268c4fe3509e472bd925164ced33a1494bbcc
Francis Carbah <fcarbah@gmail.com>
2019-08-29 00:08:36 -0400

    added closure route example

--- routes/routes.php
+++ routes/routes.php
@@ -4,3 +4,7 @@ $router = \Feather\Init\Http\Router::getInstance();
 
 
 $router->get('/','\Feather\Ignite\Controllers\WelcomeController');
+
+$router->any('/test/{west}/{:test}',function($west,$test=''){
+    echo 'You visitied test/'.$west."/$test";
+});
