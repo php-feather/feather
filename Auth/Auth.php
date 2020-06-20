004b330613293a028e96697765a82ea79b18c5e9
Francis Carbah <fcarbah@gmail.com>
2020-06-20 19:20:58 -0400

    renoved dummy licence header

--- Auth/Auth.php
+++ Auth/Auth.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace Feather\App\Auth;
 use Feather\Init\Http\Session;
 use Feather\App\Models\User;
--- Models/Model.php
+++ Models/Model.php
@@ -1,18 +1,12 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace Feather\App\Models;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model as RootModel;
 /**
  * Description of Models
  *
  * @author fcarbah
  */
-abstract class Model extends Model {
+abstract class Model extends RootModel {
     //put your code here
 }
