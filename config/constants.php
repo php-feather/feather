7bcbba3efdf9a944f7b7de73cb109a4c7a4d5b72
Francis Carbah <fcarbah@gmail.com>
2020-06-20 20:39:33 -0400

    made views parent directory and updated view path constant

--- config/constants.php
+++ config/constants.php
@@ -1,6 +1,6 @@
 <?php 
 define("BASE_PATH", dirname($_SERVER['DOCUMENT_ROOT'].'../'));
 define("STORAGE_PATH",BASE_PATH.'/storage/');
-define("VIEWS_PATH", $_SERVER['DOCUMENT_ROOT'].'/views/');
+define("VIEWS_PATH", BASE_PATH.'/views/');
 define("PUBLIC_PATH", $_SERVER['DOCUMENT_ROOT']);
 define('AUTH_USER','feather_auth_user');
