78b5a67cea587e063918b06003fe258b72bcadae
Francis Carbah <fcarbah@gmail.com>
2020-07-03 00:32:54 -0400

    set error page after registering rendering engines

--- public/index.php
+++ public/index.php
@@ -18,13 +18,13 @@ $app->init($ctrl_namespace,$default_controller,BASE_PATH.'/Controllers/');
 
 $app->setCaching();
 
-$app->setErrorPage('errors/errors.php');
-
 $app->boot([BASE_PATH.'/bootstrap/eloquent.php',BASE_PATH.'/routes/routes.php',BASE_PATH.'/helpers/view_helpers.php',BASE_PATH.'/helpers/helpers.php']);
 
 $app->registerViewEngine('native', NativeEngine::getInstance(VIEWS_PATH, BASE_PATH.'/storage/app/'));
 
 $app->registerViewEngine('twig', TwigEngine::getInstance(VIEWS_PATH, BASE_PATH.'/storage/app/'));
+
+$app->setErrorPage('errors/errors.php','native');
 
 $app->run();
 
