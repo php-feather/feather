de00d47551c86c8b16ff9c082ca6a616e9417542
Francis Carbah <fcarbah@gmail.com>
2020-12-27 23:58:15 -0500

    updated filename

--- bin/envsetup.php
+++ bin/envsetup.php
@@ -3,8 +3,8 @@
 $k = bin2hex(openssl_random_pseudo_bytes(16));
 $baseDir = dirname(__FILE__, 2);
 $sampleEnv = $baseDir . '/.env.sample';
-$contents = file_get_contents($sampleEnv);
+$contents = @file_get_contents($sampleEnv);
 $newContents = str_replace('APP_KEY=', "APP_KEY=$k", $contents);
-file_put_contents($baseDir . '/.env2', $newContents);
+return @file_put_contents($baseDir . '/.env', $newContents);
 
 
